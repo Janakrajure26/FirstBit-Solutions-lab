db86ac06e0b703eea174022540d6428fd76c072b
Janakrajure26 <130546131+Janakrajure26@users.noreply.github.com>
2025-07-01 08:00:23 +0530

    Add files via upload

---
+++ Assignment0/6_finding_square_cube_of _given _number.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int n = 8;
+	int s, c;
+	
+	s=n*n;
+	c=n*n*n;
+	 printf("square of %d is %d and cube %d", s,c);
+}
---
+++ Assignment0/8_finding_ area_of _traingle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+	 float height, base;
+	 
+	 height = 8.5;
+	 base = 6;
+	 
+	 float area = (height * base)/2;
+	 
+	 printf(" area of triangle is %0.2f", area);
+	 
+}
