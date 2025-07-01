a3209851abcb29ec7da8e424950ea853900f03bd
Janakrajure26 <130546131+Janakrajure26@users.noreply.github.com>
2025-07-01 15:21:15 +0530

    Add files via upload

---
+++ Assignment0/10_marks_of _subjects.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main ()
+{
+	float english, hindi ,maths, science,socialscience;
+	
+	printf("enter the students marks for all subjects\n");
+	
+	printf("english");
+	scanf("%f", &english);
+	
+	printf("hindi");
+	scanf("%f",& hindi);
+	
+	printf("maths");
+	scanf("%f",& maths);
+	
+	printf("science");
+	scanf("%f", & science);
+	
+	printf("socislscience");
+	scanf("%f",&socialscience);
+	
+	float total = english+ hindi+ maths+science+socialscience;
+	
+	float percentage = (total*100 )/500;
+	printf("total marks %2f/500\n", total);
+	printf("total percentage %.2f %%\n",percentage);
+	
+}
