6e72a8f45820d84874c2b635d6e85b266f2cc389
Janakrajure26 <130546131+Janakrajure26@users.noreply.github.com>
2025-07-16 02:02:06 +0530

    Add files via upload

---
+++ Assignments/Assignment_3/3.8_check_no_isStrong.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main ()
+{
+	int n, temp ,digit ,sum =0 ;
+	
+	printf("enter the number :");
+	scanf("%d", &n);
+	 temp=n;
+	 while(temp>0)
+	 {
+	 	digit =temp%10;
+	 	
+	 	int fact =1;
+	 	for (int i=1;i<=digit; i++){
+	 		fact *=i;
+		 }
+	 	sum +=fact;
+	 	temp/= 10;
+	 }
+	 if (sum ==n)
+	 {
+	 	printf("number is strong");
+	 }else{
+	 	printf(" number is not strong");
+	 }
+}
